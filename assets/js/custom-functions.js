4a537c1ef5d807aa59e01f6f2e556d10512a83e0
Eszter Mátyás <em1914@live.it>
2016-11-16 14:52:58 +0100

    Add files via upload

---
+++ assets/js/custom-functions.js
@@ -0,0 +1,202 @@
+    $('#btn_1').click(function(){
+        $('#sizing_1').toggle();
+    })
+    $('#btn_2').click(function(){
+        $('#sizing_2').toggle();
+    })
+    $('#btn_3').click(function(){
+        $('#sizing_3').toggle();
+    })
+    $('#btn_4').click(function(){
+        $('#sizing_4').toggle();
+    })
+    $('#btn_5').click(function(){
+        $('#sizing_5').toggle();
+    })
+    $('#btn_6').click(function(){
+        $('#sizing_6').toggle();
+    })
+    $('#btn_7').click(function(){
+        $('#sizing_7').toggle();
+    })
+    $('#btn_8').click(function(){
+        $('#sizing_8').toggle();
+    })
+    
+// Resize the box with buttons
+    $('#sizing_1 #hide').click(function(){
+        $('#box_1').removeClass();
+        $('#box_1').addClass('hide');
+        $('#box_1 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_1 #small').click(function(){
+        $('#box_1').removeClass();
+        $('#box_1').addClass('small');
+        $('#box_1 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_1 #medium').click(function(){
+        $('#box_1').removeClass();
+        $('#box_1').addClass('medium');
+        $('#box_1 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_1 #large').click(function(){
+        $('#box_1').removeClass();
+        $('#box_1').addClass('large');
+        $('#box_1 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_2 #hide').click(function(){
+        $('#box_2').removeClass();
+        $('#box_2').addClass('hide');
+        $('#box_2 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_2 #small').click(function(){
+        $('#box_2').removeClass();
+        $('#box_2').addClass('small');
+        $('#box_2 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_2 #medium').click(function(){
+        $('#box_2').removeClass();
+        $('#box_2').addClass('medium');
+        $('#box_2 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_2 #large').click(function(){
+        $('#box_2').removeClass();
+        $('#box_2').addClass('large');
+        $('#box_2 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_3 #hide').click(function(){
+        $('#box_3').removeClass();
+        $('#box_3').addClass('hide');
+        $('#box_3 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_3 #small').click(function(){
+        $('#box_3').removeClass();
+        $('#box_3').addClass('small');
+        $('#box_3 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_3 #medium').click(function(){
+        $('#box_3').removeClass();
+        $('#box_3').addClass('medium');
+        $('#box_3 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_3 #large').click(function(){
+        $('#box_3').removeClass();
+        $('#box_3').addClass('large');
+        $('#box_3 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_4 #hide').click(function(){
+        $('#box_4').removeClass();
+        $('#box_4').addClass('hide');
+        $('#box_4 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_4 #small').click(function(){
+        $('#box_4').removeClass();
+        $('#box_4').addClass('small');
+        $('#box_4 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_4 #medium').click(function(){
+        $('#box_4').removeClass();
+        $('#box_4').addClass('medium');
+        $('#box_4 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_4 #large').click(function(){
+        $('#box_4').removeClass();
+        $('#box_4').addClass('large');
+        $('#box_4 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_5 #hide').click(function(){
+        $('#box_5').removeClass();
+        $('#box_5').addClass('hide');
+        $('#box_5 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_5 #small').click(function(){
+        $('#box_5').removeClass();
+        $('#box_5').addClass('small');
+        $('#box_5 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_5 #medium').click(function(){
+        $('#box_5').removeClass();
+        $('#box_5').addClass('medium');
+        $('#box_5 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_5 #large').click(function(){
+        $('#box_5').removeClass();
+        $('#box_5').addClass('large');
+        $('#box_5 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_6 #hide').click(function(){
+        $('#box_6').removeClass();
+        $('#box_6').addClass('hide');
+        $('#box_6 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_6 #small').click(function(){
+        $('#box_6').removeClass();
+        $('#box_6').addClass('small');
+        $('#box_6 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_6 #medium').click(function(){
+        $('#box_6').removeClass();
+        $('#box_6').addClass('medium');
+        $('#box_6 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_6 #large').click(function(){
+        $('#box_6').removeClass();
+        $('#box_6').addClass('large');
+        $('#box_6 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_7 #hide').click(function(){
+        $('#box_7').removeClass();
+        $('#box_7').addClass('hide');
+        $('#box_7 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_7 #small').click(function(){
+        $('#box_7').removeClass();
+        $('#box_7').addClass('small');
+        $('#box_7 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_7 #medium').click(function(){
+        $('#box_7').removeClass();
+        $('#box_7').addClass('medium');
+        $('#box_7 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_7 #large').click(function(){
+        $('#box_7').removeClass();
+        $('#box_7').addClass('large');
+        $('#box_7 .col-xs-6').css( "height", "800px" );
+    })
+    
+    $('#sizing_8 #hide').click(function(){
+        $('#box_8').removeClass();
+        $('#box_8').addClass('hide');
+        $('#box_8 .col-xs-6').css( "height", "0px", "width", "0px" );
+    })                     
+    $('#sizing_8 #small').click(function(){
+        $('#box_8').removeClass();
+        $('#box_8').addClass('small');
+        $('#box_8 .col-xs-6').css( "height", "200px" );
+    })
+    $('#sizing_8 #medium').click(function(){
+        $('#box_8').removeClass();
+        $('#box_8').addClass('medium');
+        $('#box_8 .col-xs-6').css( "height", "400px" );
+    })
+    $('#sizing_8 #large').click(function(){
+        $('#box_8').removeClass();
+        $('#box_8').addClass('large');
+        $('#box_8 .col-xs-6').css( "height", "800px" );
+    })
+    
+    //sortable menu
+    
+    $(function() {
+    $('#sortable').sortable();
+    $('#sortable').disableSelection();
+    });
+
+    //sort visualizations
