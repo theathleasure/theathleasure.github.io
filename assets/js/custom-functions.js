a9ccccb8eab8c03abe50d1ff7d1d3d86327b9b6e
Eszter Mátyás <em1914@live.it>
2016-11-17 17:07:33 +0100

    Update custom-functions.js

--- assets/js/custom-functions.js
+++ assets/js/custom-functions.js
@@ -301,86 +301,10 @@
 
     //sort visualizations
 function handleDragStart(e) {
-  this.style.border = '2px dashed #000';
+  this.style.size = '60%';
 }
 
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
-  }
-
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
-  return false;
-}
-
-function handleDragEnter(e) {
-  // this / e.target is the current hover target.
-  this.classList.add('over');
-}
-
-function handleDragLeave(e) {
-  this.classList.remove('over');  // this / e.target is previous target element.
-}
-
-var cols = document.querySelectorAll('.row .col-sm-12');
+var cols = document.querySelectorAll('#row .col-sm-12');
 [].forEach.call(cols, function(col) {
   col.addEventListener('dragstart', handleDragStart, false);
-  col.addEventListener('dragenter', handleDragEnter, false);
-  col.addEventListener('dragover', handleDragOver, false);
-  col.addEventListener('dragleave', handleDragLeave, false);
 });
-
-function handleDrop(e) {
-
-  if (e.stopPropagation) {
-    e.stopPropagation(); // stops the browser from redirecting.
-  }
-
-  return false;
-}
-
-function handleDragEnd(e) {
-
-  [].forEach.call(cols, function (col) {
-    col.classList.remove('over');
-  });
-}
-
-var cols = document.querySelectorAll('#columns .column');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
-  col.addEventListener('dragenter', handleDragEnter, false)
-  col.addEventListener('dragover', handleDragOver, false);
-  col.addEventListener('dragleave', handleDragLeave, false);
-  col.addEventListener('drop', handleDrop, false);
-  col.addEventListener('dragend', handleDragEnd, false);
-});
-
-var dragSrcEl = null;
-
-function handleDragStart(e) {
-  this.style.border = '2px dashed #000';
-
-  dragSrcEl = this;
-
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-function handleDrop(e) {
-  // this/e.target is current target element.
-
-  if (e.stopPropagation) {
-    e.stopPropagation(); // Stops some browsers from redirecting.
-  }
-
-  // Don't do anything if dropping the same column we're dragging.
-  if (dragSrcEl != this) {
-    // Set the source column's HTML to the HTML of the column we dropped on.
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
-  }
-
-  return false;
-}
